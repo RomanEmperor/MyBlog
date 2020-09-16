c66f9ad3ba4791b1c7308745730f0bc90b2f1b61
Roman Parajuli <notorious.garage@gmail.com>
2020-09-16 17:09:23 +0545

    First commit

---
+++ src/build/precache-manifest.0e575afcf584eb33b5ae61a49fa81d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bf05d10158511d3604b9c21b96ed07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2166c4284e79d227504e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "b3137f8f841162dc5b2f",
+    "url": "/static/js/2.45200934.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45200934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2166c4284e79d227504e",
+    "url": "/static/js/main.11066804.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
